d4daa3d22dd253fcd9222988084a353034fd2365
jakub.wszola <jakub.wszola@asseco.pl>
2023-03-17 18:30:50 +0100

    # config server

---
+++ ConfigServer/src/test/java/com/brokagehouse/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokagehouse.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
