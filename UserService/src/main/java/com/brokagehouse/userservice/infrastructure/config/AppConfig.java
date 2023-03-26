4d074fb63d0ef3d1858bb186f597dffca07a217b
jakub.wszola <jakub.wszola@asseco.pl>
2023-03-26 20:27:43 +0200

    app works

--- UserService/src/main/java/com/brokagehouse/userservice/infrastructure/config/AppConfig.java
+++ UserService/src/main/java/com/brokagehouse/userservice/infrastructure/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.brokagehouse.userservice.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,5 +14,10 @@ public class AppConfig {
     @Bean
     public ExecutorService executorService() {
         return Executors.newFixedThreadPool(10);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://auth-service:8092").build();
     }
 }
