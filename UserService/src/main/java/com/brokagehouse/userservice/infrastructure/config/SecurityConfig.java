d810616bb43d2947cb12f849d125d2f1d882e8e3
jakub.wszola <jakub.wszola@asseco.pl>
2023-03-30 11:32:16 +0200

    fix

--- UserService/src/main/java/com/brokagehouse/userservice/infrastructure/config/SecurityConfig.java
+++ UserService/src/main/java/com/brokagehouse/userservice/infrastructure/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.brokagehouse.userservice.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,35 +12,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.mvcMatcher("/**")
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.POST, "/user/create") // Endpoint POST /user/create będzie wykluczony z zabezpieczeń
-//                .authenticated()
-//                .mvcMatchers("/**")
-//                .access("hasAuthority('SCOPE_message.read')")
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt();
-
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/user/home").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/user/create").permitAll()
-                        .anyRequest().authenticated()
-                );
-
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/user/create").permitAll();
         return http.build();
-//				.requestMatchers("/web/app/**").authenticated()
-//				.and()
-//				.formLogin().loginPage("/web/login/login-page")
-//				.loginProcessingUrl("/authenticateUser")
-//				.defaultSuccessUrl("/web/app/dashboard")
-//				.permitAll()
-//				.and()
-//				.logout()
-//				.logoutSuccessUrl("/web/login/login-page?logout")
-//				.permitAll().and().build();
-
-//        return http.build();
     }
 }
